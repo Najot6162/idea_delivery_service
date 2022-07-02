bc4a8520d73076b287cc038db392326249f3dd7a
Biloliddin <najot.6162@gmail.com>
2022-07-02 12:20:50 +0500

    created project

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+            'login'=> 'required',
+            'password' =>'required'
+        ]);
+        if($validator->fails())
+        {
+            return response()->json([
+                'status_code'=>400,
+                'message'=>'Bad Request'
+            ]);
+
+        }
+
+        $credentials = request(['login','password']);
+
+        if(!Auth::attempt($credentials))
+        {
+            return response()->json([
+                'status_code'=>500,
+                'message'=>'Unauthorized'
+            ]);
+        }
+ 
+        $user = User::where('login',$request->login)->first();
+
+        $tokenResult = $user->createToken('authToken')->plainTextToken;
+
+        return response()->json([
+            'message'=>$tokenResult
+        ]);
+    }
+    public function logout(Request $request)
+    {
+        if($request->user()){
+            $request->user()->tokens()->delete();
+        }
+        echo $request;
+        // Auth::logout();
+        // // $request->user()->currentAccessToken()->delete();
+        return response() ->json([
+            'message' => 'Token deleted successfully'
+        ]);
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = [
+            [
+                'login'=>'dell',
+                'password'=>bcrypt('dell123'),
+                'role'=>'admin'
+            ]
+            ];
+
+            foreach ($users as $key => $value){
+                User::create($value);
+            }
+    }
+}
