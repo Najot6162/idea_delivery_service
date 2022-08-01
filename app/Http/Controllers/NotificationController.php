4c1797f711681b881fea26e9ba2ab2b582fb07dd
Biloliddin <najot6162@gmail.com>
2022-08-01 16:15:11 +0500

    bugfix notification function in notificationController

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -8,9 +8,11 @@ use Illuminate\Support\Facades\Validator;
 
 class NotificationController extends Controller
 {
-    public function sendNotification(Request $request)
+    public function sendNotification($user_id)
     {
-        $firebaseToken = User::whereNotNull('fcm_token')->pluck('fcm_token')->where('id','user_id')->all();
+        $user = User::findOrFail($user_id);
+        $firebaseToken = array($user->fcm_token);
+        //$firebaseToken = ['fXQmFhuGSDCaAr16F9rOok:APA91bGNxTCQ6_-oi1Hvc076ZJl8nwT5pKjqVUgVjtc2QM5ySeA-6yqtIOwvS8pVvnWKDz2wRCB-3g93LIZHE-HwrtHsXnDtkihVHDl1u8abMbfi7d1vHxlNrIE2-YZ3wgJknj1Ydvg_'];
 
         $SERVER_API_KEY = 'AAAAgkmO-9I:APA91bF0-yitF0ni_yVGEma5eaP_K_1-BlDcpbkT6h874w4EXzY0X8PtS6Bm0Oo9-DIBQm8XUxYzis1UGq9PRqZCXPf4MlR0wQDnBiLIVd3TU_2jdGu4qL_U0K6E7bMFDHJWKjpD-WQ6';
 
@@ -38,7 +40,7 @@ class NotificationController extends Controller
         curl_setopt($ch, CURLOPT_POSTFIELDS, $dataString);
 
         $response = curl_exec($ch);
-
+        print_r($response);
         return back()->with('success', 'Notification send successfully.');
     }
 
