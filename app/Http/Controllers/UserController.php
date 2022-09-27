3c4016d12951aa35bc4a3b8461918cd64e616843
Najot6162 <najot.6162@gmail.com>
2022-09-27 14:16:42 +0500

    bugfix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -179,9 +179,6 @@ class UserController extends Controller
         if ($request->branch_id) {
             $delviery->whereIn('branch_id', $request->branch_id);
         }
-        if ($request->status){
-            $delviery->status = $request->status;
-        }
         return BranchResource::collection($delviery->paginate($request->perPage));
     }
 
