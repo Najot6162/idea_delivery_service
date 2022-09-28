7766bccaa002409fe5ef26732d765bfe2d994a1e
Najot6162 <najot.6162@gmail.com>
2022-09-28 14:20:14 +0500

    added passport auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,7 +7,9 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
-
+use Carbon\Carbon;
+use Laravel\Passport\RefreshTokenRepository;
+use Illuminate\Support\Facades\Http;
 
 class AuthController extends Controller
 {
@@ -25,34 +27,39 @@ class AuthController extends Controller
         }
         $credentials = request(['phone', 'password']);
 
-        if (!Auth::attempt($credentials)) {
-            return response()->json([
-                'status_code' => 400,
-                'message' => 'Bad Request'
-            ], 400);
+        if (!auth()->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
         }
         $user = User::with(['userPermission', 'userPermission.menus', 'carModel'])->where('phone', $request->phone)->first();
-        $tokenResult = $user->createToken('authToken')->plainTextToken;
+        $tokenData = auth()->user()->createToken('MyApiToken');
+        $token = $tokenData->accessToken;
+        $expiration = $tokenData->token->expires_at->diffInSeconds(Carbon::now());
         return response()->json([
-            'token' => $tokenResult,
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+            'expires_in' => $expiration,
             'user' => $user
         ]);
     }
 
     public function logout(Request $request)
     {
-        if ($request->user()) {
-            $request->user()->tokens()->delete();
+        $token = auth()->user()->token();
+
+        /* --------------------------- revoke access token -------------------------- */
+        $token->revoke();
+        $token->delete();
+
             if ($request->user()->fcm_token) {
-                $user = User::findOrFail($request->user()->id);
-                $user->fcm_token = null;
-                $user->save();
-            }
+            $user = User::findOrFail($request->user()->id);
+            $user->fcm_token = null;
+            $user->save();
         }
-        return response()->json([
-            'status_code' => 200,
-            'message' => 'logout'
-        ], 200);
+        /* -------------------------- revoke refresh token -------------------------- */
+        $refreshTokenRepository = app(RefreshTokenRepository::class);
+        $refreshTokenRepository->revokeRefreshTokensByAccessTokenId($token->id);
+
+        return response()->json(['message' => 'Logged out successfully']);
     }
 
     public function getAuthUser()
@@ -63,4 +70,61 @@ class AuthController extends Controller
             'user' => $user
         ]);
     }
+
+    public function loginGrant(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'phone' => 'required',
+            'password' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $user = User::with(['userPermission', 'userPermission.menus', 'carModel'])->where('phone', $request->phone)->first();
+        $baseUrl = url('http://devserv.gx.uz');
+        $response = Http::post("{$baseUrl}/oauth/token", [
+            'username' => $request->email,
+            'password' => $request->password,
+            'client_id' => config('passport.password_grant_client.id'),
+            'client_secret' => config('passport.password_grant_client.secret'),
+            'grant_type' => 'password'
+        ]);
+
+        $result = json_decode($response->getBody(), true);
+        if (!$response->ok()) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+        return response()->json([
+            'result'=>$result,
+            'user' => $user,
+            "data"=>"ok"
+        ]);
+    }
+    public function refreshToken(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'refresh_token' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $baseUrl = url('/');
+        $response = Http::post("{$baseUrl}/oauth/token", [
+            'refresh_token' => $request->refresh_token,
+            'client_id' => config('passport.password_grant_client.id'),
+            'client_secret' => config('passport.password_grant_client.secret'),
+            'grant_type' => 'refresh_token'
+        ]);
+
+        $result = json_decode($response->getBody(), true);
+        if (!$response->ok()) {
+            return response()->json(['error' => $result['error_description']], 401);
+        }
+        return response()->json($result);
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,7 @@ use \App\Http\Controllers\ProblemController;
 use \App\Http\Controllers\NotificationController;
 use \App\Http\Controllers\FileController;
 use \Illuminate\Support\Facades\Broadcast;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -26,110 +27,121 @@ use \Illuminate\Support\Facades\Broadcast;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Broadcast::routes(['middleware'=>'auth:api']);
 
-Route::group(['middleware' => ['auth:sanctum']], function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
+Broadcast::routes(['middleware' => 'auth:api']);
 
-    //Delivery Api
-    Route::post('/create-delivery', [DeliveryController::class, 'CreateDelivery']);
-    Route::get('/get-all-delivery', [DeliveryController::class, 'gettAllDelivery']);//->middleware(['admin']);
-    Route::put('/update-delivery/{id}', [DeliveryController::class, 'updateDelivery']); //->middleware(['admin']);
-    Route::put('/back-step/{id}', [DeliveryController::class, 'backStep']);
-    Route::put('/update-pickup-time/{id}', [DeliveryController::class, 'updatePickupTime']);
-    //File Upload
-    Route::post('/upload_file', [FileController::class, 'uploadFile']);//->middleware(['admin']);
+Route::group(['namespace' => 'Api'], function () {
 
-    //ConfigTime Api
-    Route::get('/check-time', [ConfigTimeController::class, 'checkTime']);
-    Route::post('/crete-config-time', [ConfigTimeController::class, 'creteConfigTime']);
-    Route::put('/update-config-time', [ConfigTimeController::class, 'updateConfigTime']);
-    Route::get('/get-all-config-time', [ConfigTimeController::class, 'getAllConfigTime']);
+    Route::post('/login', [AuthController:: class, 'login']);
+    Route::post('/login_grant', [AuthController::class, 'loginGrant']);
+    Route::get('/download-image/{url}', [FileController::class, 'downloadImageFile']);
 
-    //Branch Api
-    Route::post('/create-branch', [BranchController::class, 'createBranch']);
-    Route::put('/update-branch/{id}', [BranchController::class, 'updateBranch']);
-    Route::get('/get-all-branchs', [BranchController::class, 'getAllBranch']);
-    Route::get('/get-all-regions', [BranchController::class, 'getAllBranchRegion']);
-    Route::post('/create-region',[BranchController::class,'createRegion']);
-    //Relocation Api
-    Route::post('/create-relocation', [RelocationController::class, 'CreateRelocation']);
-    Route::get('/get-all-relocation', [RelocationController::class, 'getAllRelocation']);
-    Route::put('/update-relocation/{id}', [RelocationController::class, 'updateRelocation']);
-    Route::get('/get-relocation/{id}', [RelocationController::class, 'getRelocation']);
+    Route::group(['middleware' => 'auth:api'], function () {
+        Route::post('/logout', [AuthController::class, 'logout']);
 
-    //Driver Api
-    Route::post('/create-driver', [UserController::class, 'createDriver']);
-    Route::put('/update-driver/{id}', [UserController::class, 'updateDriver']);
-    Route::get('/get-all-driver', [UserController::class, 'getAllDrivers']);//->middleware(['driver']);
-    Route::get('/get-delivery/{id}', [UserController::class, 'getDelivery']);
-    Route::get('/get-driver-only-active', [UserController::class, 'getAllDriversOnlyActive']);
-    Route::get('/get-driver/{id}', [UserController::class, 'getDriver']);
+        //Delivery Api
+        Route::post('/create-delivery', [DeliveryController::class, 'CreateDelivery']);
+        Route::get('/get-all-delivery', [DeliveryController::class, 'gettAllDelivery']);//->middleware(['admin']);
+        Route::put('/update-delivery/{id}', [DeliveryController::class, 'updateDelivery']); //->middleware(['admin']);
+        Route::put('/back-step/{id}', [DeliveryController::class, 'backStep']);
+        Route::put('/update-pickup-time/{id}', [DeliveryController::class, 'updatePickupTime']);
+        //File Upload
+        Route::post('/upload_file', [FileController::class, 'uploadFile']);//->middleware(['admin']);
 
-    //Car Api
-    Route::post('/create-car', [CarController::class, 'createCar']);
-    Route::put('/update-car/{id}', [CarController::class, 'updateCar']);
-    Route::get('/get-all-car', [CarController::class, 'getAllCars']);
-    Route::get('/get-car-term/{id}', [CarController::class, 'getCarTerm']);
-    Route::put('/update-car-term/{id}', [CarController::class, 'updateCarTerm']);
-    Route::delete('/delete-car/{id}', [CarController::class, 'deleteCar']);
-    Route::put('/update-active-car/{id}', [CarController::class, 'updateOnlyActiveCar']);
-    Route::get('/get-car/{id}', [CarController::class, 'getCar']);
+        //ConfigTime Api
+        Route::get('/check-time', [ConfigTimeController::class, 'checkTime']);
+        Route::post('/crete-config-time', [ConfigTimeController::class, 'creteConfigTime']);
+        Route::put('/update-config-time', [ConfigTimeController::class, 'updateConfigTime']);
+        Route::get('/get-all-config-time', [ConfigTimeController::class, 'getAllConfigTime']);
 
-//    //Call Center Api
+        //Branch Api
+        Route::post('/create-branch', [BranchController::class, 'createBranch']);
+        Route::put('/update-branch/{id}', [BranchController::class, 'updateBranch']);
+        Route::get('/get-all-branchs', [BranchController::class, 'getAllBranch']);
+        Route::get('/get-all-regions', [BranchController::class, 'getAllBranchRegion']);
+        Route::post('/create-region', [BranchController::class, 'createRegion']);
+        //Relocation Api
+        Route::post('/create-relocation', [RelocationController::class, 'CreateRelocation']);
+        Route::get('/get-all-relocation', [RelocationController::class, 'getAllRelocation']);
+        Route::put('/update-relocation/{id}', [RelocationController::class, 'updateRelocation']);
+        Route::get('/get-relocation/{id}', [RelocationController::class, 'getRelocation']);
+
+        //Driver Api
+        Route::post('/create-driver', [UserController::class, 'createDriver']);
+        Route::put('/update-driver/{id}', [UserController::class, 'updateDriver']);
+        Route::get('/get-all-driver', [UserController::class, 'getAllDrivers']);//->middleware(['driver']);
+        Route::get('/get-delivery/{id}', [UserController::class, 'getDelivery']);
+        Route::get('/get-driver-only-active', [UserController::class, 'getAllDriversOnlyActive']);
+        Route::get('/get-driver/{id}', [UserController::class, 'getDriver']);
+
+        //Car Api
+        Route::post('/create-car', [CarController::class, 'createCar']);
+        Route::put('/update-car/{id}', [CarController::class, 'updateCar']);
+        Route::get('/get-all-car', [CarController::class, 'getAllCars']);
+        Route::get('/get-car-term/{id}', [CarController::class, 'getCarTerm']);
+        Route::put('/update-car-term/{id}', [CarController::class, 'updateCarTerm']);
+        Route::delete('/delete-car/{id}', [CarController::class, 'deleteCar']);
+        Route::put('/update-active-car/{id}', [CarController::class, 'updateOnlyActiveCar']);
+        Route::get('/get-car/{id}', [CarController::class, 'getCar']);
+
+//      Call Center Api
 //    Route::get('/get-all-call-delivery', [CallCenterController::class, 'getAllCallDelivery']);
 
-    //Service Api
-    Route::post('/create-problem', [ProblemController::class, 'createProblem']);
-    Route::get('/get-all-problems', [ProblemController::class, 'getAllProblems']);
-    Route::put('/update-problem/{id}', [ProblemController::class, 'updateProblem']);
-    Route::get('/get-problem-item/{id}', [ProblemController::class, 'getProblemItem']);
-    Route::post('/add-comment', [ProblemController::class, 'addCommentToTimeStep']);
+        //Service Api
+        Route::post('/create-problem', [ProblemController::class, 'createProblem']);
+        Route::get('/get-all-problems', [ProblemController::class, 'getAllProblems']);
+        Route::put('/update-problem/{id}', [ProblemController::class, 'updateProblem']);
+        Route::get('/get-problem-item/{id}', [ProblemController::class, 'getProblemItem']);
+        Route::post('/add-comment', [ProblemController::class, 'addCommentToTimeStep']);
 
-    Route::get('/get-problem/{id}', [ProblemController::class, 'getProblem']);
+        Route::get('/get-problem/{id}', [ProblemController::class, 'getProblem']);
 
-    //Service Center
-    Route::post('/create-problem-service', [ProblemController::class, 'createProblemService']);
-    Route::put('/update-problem-service/{id}', [ProblemController::class, 'updateProblemService']);
-    Route::get('/get-all-problem-service', [ProblemController::class, 'getAllProblemServices']);
-    Route::get('/get-problem-service/{id}', [ProblemController::class, 'getProblemService']);
-    Route::delete('/problem-service/{id}', [ProblemController::class, 'deleteProblemService']);
-    Route::get('/get-all-users-with-branch', [UserController::class, 'getAllUsersWithBranch']);
-    Route::put('/update-user-branch/{id}', [UserController::class, 'updateUserBranch']);
+        //Service Center
+        Route::post('/create-problem-service', [ProblemController::class, 'createProblemService']);
+        Route::put('/update-problem-service/{id}', [ProblemController::class, 'updateProblemService']);
+        Route::get('/get-all-problem-service', [ProblemController::class, 'getAllProblemServices']);
+        Route::get('/get-problem-service/{id}', [ProblemController::class, 'getProblemService']);
+        Route::delete('/problem-service/{id}', [ProblemController::class, 'deleteProblemService']);
+        Route::get('/get-all-users-with-branch', [UserController::class, 'getAllUsersWithBranch']);
+        Route::put('/update-user-branch/{id}', [UserController::class, 'updateUserBranch']);
 
-    //Dashboard Api
-    Route::get('/get-counts', [DashboardController::class, 'getCounts']);
+        //Dashboard Api
+        Route::get('/get-counts', [DashboardController::class, 'getCounts']);
 
-    //Permission Api
-    Route::get('/get-roles_group', [UserController::class, 'roleGroup']);
-    Route::get('/get-permission', [UserController::class, 'getPermission']);
-    Route::put('/update-permission', [UserController::class, 'updatePermission']);
-    Route::get('/get-menus', [UserController::class, 'getMenus']);
-    Route::post('/create-role-permission', [UserController::class, 'createRoleWithPermission']);
+        //Permission Api
+        Route::get('/get-roles_group', [UserController::class, 'roleGroup']);
+        Route::get('/get-permission', [UserController::class, 'getPermission']);
+        Route::put('/update-permission', [UserController::class, 'updatePermission']);
+        Route::get('/get-menus', [UserController::class, 'getMenus']);
+        Route::post('/create-role-permission', [UserController::class, 'createRoleWithPermission']);
 
-    //Users Api
-    Route::get('/get-users', [UserController::class, 'getUsers']);
-    Route::put('/update-user/{id}', [UserController::class, 'updateUser']);
-    Route::put('/update-user-active/{id}', [UserController::class, 'updateUserActive']);
-    Route::get('/get-user/{id}', [UserController::class, 'getUser']);
-    Route::get('/get-all-users', [UserController::class, 'getAllUsers']);
-    Route::post('/create-user', [UserController::class, 'createUser']);
-    Route::post('/create-fcm-token/{id}', [NotificationController::class, 'createFcmToken']);
-    Route::get('/get-auth-user', [AuthController::class, 'getAuthUser']);
-    Route::put('/update-user-deleted/{id}', [UserController::class, 'UserIsDelete']);
+        //Users Api
+        Route::get('/get-users', [UserController::class, 'getUsers']);
+        Route::put('/update-user/{id}', [UserController::class, 'updateUser']);
+        Route::put('/update-user-active/{id}', [UserController::class, 'updateUserActive']);
+        Route::get('/get-user/{id}', [UserController::class, 'getUser']);
+        Route::get('/get-all-users', [UserController::class, 'getAllUsers']);
+        Route::post('/create-user', [UserController::class, 'createUser']);
+        Route::post('/create-fcm-token/{id}', [NotificationController::class, 'createFcmToken']);
+        Route::get('/get-auth-user', [AuthController::class, 'getAuthUser']);
+        Route::put('/update-user-deleted/{id}', [UserController::class, 'UserIsDelete']);
 
+    });
     //Api for Mobile app
+    Route::get('/download-app', [FileController::class, 'getDownload']);
+    Route::get('/read-json-app-file', [FileController::class, 'readAppJsonFile']);
+    Route::get('/get-mobile-menus/{id}', [UserController::class, 'getPermissionForMobile']);
     Route::post('/upload-app', [FileController::class, 'uploadAppFile']);
-
 });
-Route::get('/download-image/{url}', [FileController::class, 'downloadImageFile']);
-//Api for Mobile app
-Route::get('/download-app', [FileController::class, 'getDownload']);
-Route::get('/read-json-app-file', [FileController::class, 'readAppJsonFile']);
 
-Route::get('/get-mobile-menus/{id}', [UserController::class, 'getPermissionForMobile']);
+Route::get('unauthorized', function () {
+    return response()->json([
+        'error' => 'Unauthorized.'
+    ], 401);
+})->name('unauthorized');
 
-Route::post('/login', [AuthController:: class, 'login']);
+
+
